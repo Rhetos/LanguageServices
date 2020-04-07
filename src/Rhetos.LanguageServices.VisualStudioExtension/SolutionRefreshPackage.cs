1cebbe39f49322b6e3c8257d829a88a2df06048a
Sasa Stublic <sasa.stublic@gmail.com>
2020-04-07 21:00:08 +0200

    Functional OnBuildDone event mock.

---
+++ src/Rhetos.LanguageServices.VisualStudioExtension/SolutionRefreshPackage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Rhetos.LanguageServices.VisualStudioExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("My Asynchronous Package", "Loads asynchronously", "1.0")]
+    [ProvideService(typeof(SolutionRefreshService), IsAsyncQueryable = true)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid("c4ddd728-d28b-49fe-bdd9-fdfd590d4391")]
+    public sealed class SolutionRefreshPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // Adds a service on the background thread
+            AddService(typeof(SolutionRefreshService), CreateMyServiceAsync);
+
+            var svc = await GetServiceAsync(typeof(SolutionRefreshService));
+        }
+
+        private async Task<object> CreateMyServiceAsync(IAsyncServiceContainer container, CancellationToken cancellationToken, Type serviceType)
+        {
+            var svc = new SolutionRefreshService();
+            await svc.InitializeAsync(this, cancellationToken);
+            return svc;
+        }
+    }
+}
---
+++ src/Rhetos.LanguageServices.VisualStudioExtension/SolutionRefreshService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using IAsyncServiceProvider = Microsoft.VisualStudio.Shell.IAsyncServiceProvider;
+using Task = System.Threading.Tasks.Task;
+
+namespace Rhetos.LanguageServices.VisualStudioExtension
+{
+    public class SolutionRefreshService
+    {
+        private DTE2 dte;
+
+        public async Task InitializeAsync(IAsyncServiceProvider provider, CancellationToken cancellationToken)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            dte = (DTE2) await provider.GetServiceAsync(typeof(DTE));
+            Assumes.Present(dte);
+            dte.Events.BuildEvents.OnBuildDone += BuildEvents_OnBuildDone;
+            await WriteToOutputWindowAsync(nameof(SolutionRefreshService), "Initialized!");
+        }
+
+        private void BuildEvents_OnBuildDone(vsBuildScope scope, vsBuildAction action)
+        {
+            ThreadHelper.JoinableTaskFactory.Run(() => WriteToOutputWindowAsync(nameof(SolutionRefreshService), "Build Done!"));
+        }
+
+        private async Task WriteToOutputWindowAsync(string paneName, string message)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var panes = dte.ToolWindows.OutputWindow.OutputWindowPanes;
+            OutputWindowPane pane;
+            try
+            {
+                pane = panes.Item(paneName);
+            }
+            catch (ArgumentException)
+            {
+                pane = panes.Add(paneName);
+            }
+
+            pane.Activate();
+            pane.OutputString(message);
+            pane.OutputString(Environment.NewLine);
+        }
+    }
+}
