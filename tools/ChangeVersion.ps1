f3d43fdc7c591dfa9276640cb9281ef83b71b729
Bojan Antolović <bojan.antolovic@gmail.com>
2022-03-24 13:46:38 +0100

    Mached build versions for all projects.

---
+++ tools/ChangeVersion.ps1
@@ -0,0 +1,33 @@
+param([string]$version, [string]$prerelease)
+
+If ($prerelease -eq 'auto')
+{
+    $prerelease = ('dev'+(get-date -format 'yyMMddHHmm')+(git rev-parse --short HEAD)).Substring(0,20)
+}
+
+If ($prerelease)
+{
+    $fullVersion = $version + '-' + $prerelease
+}
+Else
+{
+    $fullVersion = $version
+}
+
+function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
+{
+    Get-ChildItem $fileSearch -r `
+        | ForEach-Object {
+        $c = [IO.File]::ReadAllText($_.FullName, [System.Text.Encoding]::Default) -Replace $replacePattern, $replaceWith;
+        [IO.File]::WriteAllText($_.FullName, $c, [System.Text.Encoding]::UTF8)
+    }
+}
+
+Write-Output "Setting version '$fullVersion'."
+
+RegexReplace '*AssemblyInfo.cs' '([\n^]\[assembly: Assembly(File)?Version(Attribute)?\(\").*(\"\)\])' ('${1}' + $version + '${4}')
+RegexReplace '*AssemblyInfo.cs' '([\n^]\[assembly: AssemblyInformationalVersion(Attribute)?\(\").*(\"\)\])' ('${1}' + $fullVersion + '${3}')
+RegexReplace '*.nuspec' '([\n^]\s*\<version\>).*(\<\/version\>\s*)' ('${1}' + $fullVersion + '${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<InformationalVersion\>).*(\<\/InformationalVersion\>\s*)' ('${1}' + $fullVersion + '${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<AssemblyVersion\>).*(\<\/AssemblyVersion\>\s*)' ('${1}' + $version + '${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<FileVersion\>).*(\<\/FileVersion\>\s*)' ('${1}' + $version + '${2}')
