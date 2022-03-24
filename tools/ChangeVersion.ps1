f318ed7729fd63c66d388aa1e7968c5d001f7748
Bojan Antolović <bojan.antolovic@gmail.com>
2022-03-24 14:34:06 +0100

    ChangeVerion.ps1: Added versioning for VSIX and MSI.

--- tools/ChangeVersion.ps1
+++ tools/ChangeVersion.ps1
@@ -1,18 +1,45 @@
 param([string]$version, [string]$prerelease)
 
-If ($prerelease -eq 'auto')
+$ErrorActionPreference = 'Stop'
+
+[string[]]$exclude = @('bin', 'obj', 'dist', '.*', 'Logs')
+[string[]]$folders = @()
+[string[]]$foldersNew = Get-Location
+while ($foldersNew.Count -gt 0)
 {
-    $prerelease = ('dev'+(get-date -format 'yyMMddHHmm')+(git rev-parse --short HEAD)).Substring(0,20)
+    [string[]]$folders += $foldersNew
+    [string[]]$foldersNew = Get-ChildItem $foldersNew -Directory -Exclude $exclude
+}
+$folders = $folders | Sort-Object
+
+function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
+{
+    Get-ChildItem -File -Path $folders -Filter $fileSearch `
+        | Select-Object -ExpandProperty FullName `
+        | ForEach-Object {
+            $text = [IO.File]::ReadAllText($_, [System.Text.Encoding]::UTF8)
+            $replaced = $text -Replace $replacePattern, $replaceWith
+            if ($replaced -ne $text) {
+                Write-Output $_
+                [IO.File]::WriteAllText($_, $replaced, [System.Text.Encoding]::UTF8)
+            }
+        }
 }
 
-If ($prerelease)
+If ($prerelease -eq 'auto')
 {
-    $fullVersion = $version + '-' + $prerelease
+    $prereleaseSuffix = ('-dev' + (get-date -format 'yyMMddHHmm') + (git rev-parse --short HEAD)).Substring(0,20)
+}
+ElseIf ($prerelease)
+{
+    $prereleaseSuffix = '-' + $prerelease
 }
 Else
 {
-    $fullVersion = $version
+    $prereleaseSuffix = ''
 }
+$fullVersion = $version.ToString() + $prereleaseSuffix
+Write-Output "Setting version '$fullVersion'."
 
 function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
 {
@@ -31,3 +58,5 @@ RegexReplace '*.nuspec' '([\n^]\s*\<version\>).*(\<\/version\>\s*)' ('${1}' + $f
 RegexReplace 'Directory.Build.props' '([\n^]\s*\<InformationalVersion\>).*(\<\/InformationalVersion\>\s*)' ('${1}' + $fullVersion + '${2}')
 RegexReplace 'Directory.Build.props' '([\n^]\s*\<AssemblyVersion\>).*(\<\/AssemblyVersion\>\s*)' ('${1}' + $version + '${2}')
 RegexReplace 'Directory.Build.props' '([\n^]\s*\<FileVersion\>).*(\<\/FileVersion\>\s*)' ('${1}' + $version + '${2}')
+RegexReplace 'source.extension.vsixmanifest' '(\<Identity.*Version=\").*?(\")' ('${1}' + $version + '${2}')
+RegexReplace 'RhetosLanguageServicesInstaller.vdproj' '(\"ProductVersion\" = \"8:).*?(\")' ('${1}' + $version + '${2}')
